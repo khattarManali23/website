58bf567ff84a322d3e9996f609d3a4132b3ee01d
= <=>
2022-12-16 16:18:00 +0530

    change in portfolio

--- components/Mansory.js
+++ components/Mansory.js
@@ -5,16 +5,12 @@ const Mansory = ({ images }) => {
   return (
     <div>
       <div
-        class="columns-2 border border-red-900
-       gap-8   "
+        class="columns-2 md:columns-2 lg:columns-2
+       gap-4  min-w-[350px]  "
       >
         {images.map((image, index) => {
           return (
-            <div
-              className="flex justify-center items-center mb-4  
-              "
-              key={index}
-            >
+            <div className="flex justify-center items-center mb-4" key={index}>
               {console.log(image, "image")}
               <img
                 src={image.src}
