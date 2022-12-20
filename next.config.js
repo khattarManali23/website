6f876b0431c97d6759aa9379d6e6e50000628f49
khattarmanali23 <dev17.technolitics@gmail.com>
2022-12-21 00:00:18 +0530

    chnges

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  fontLoaders: [{ loader: "@next/font/google" }],
+  // fontLoaders: [{ loader: "@next/font/google" }],
 
   reactStrictMode: true,
   swcMinify: true,
