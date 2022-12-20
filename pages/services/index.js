2787818d89d34f0241e31489cf4afc0203d06c55
khattarmanali23 <dev17.technolitics@gmail.com>
2022-12-20 18:36:14 +0530

    contaqct us done

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../../components/Banner";
+import PageFooter from "../../components/PageFooter";
 import ServiceCard from "../../components/ServiceCard";
 import data from "../../data/banner";
 
@@ -9,11 +10,13 @@ const Services = () => {
     <div>
       <Banner banner={banner} />
       <div className="flex justify-center flex-wrap my-4 items-center">
-        <h1 className="text-5xl leading-[67.2px]  w-1/4 text-center text-transparent bg-clip-text  sm:bg-gradient-to-r sm:from-[#FFB838] sm:via-[#F34F8C] sm:to-[#8236BA]  bg-gradient-to-r from-[#8236BA] via-[#F34F8C] to-[#FFB838] ">
+        <h1 className="text-5xl leading-[67.2px] w-full sm:w-1/4 text-center text-transparent bg-clip-text  sm:bg-gradient-to-r sm:from-[#FFB838] sm:via-[#F34F8C] sm:to-[#8236BA]  bg-gradient-to-r from-[#8236BA] via-[#F34F8C] to-[#FFB838] ">
           DOMAIN
         </h1>
 
         <ServiceCard />
+
+        <PageFooter />
       </div>
     </div>
   );
