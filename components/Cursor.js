112ad6158ae37da60da32232695f5650ef200263
khattarmanali23 <dev17.technolitics@gmail.com>
2022-12-24 13:12:44 +0530

    changes in career page

---
+++ components/Cursor.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Cursor = () => {
+  const { useEffect, useState } = React;
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    addEventListeners();
+    return () => removeEventListeners();
+  }, []);
+
+  const addEventListeners = () => {
+    document.addEventListener("mousemove", onMouseMove);
+  };
+
+  const removeEventListeners = () => {
+    document.removeEventListener("mousemove", onMouseMove);
+  };
+
+  const onMouseMove = (e) => {
+    setPosition({ x: e.pageX, y: e.pageY });
+  };
+
+  console.log(position.x);
+
+  return (
+    <div>
+      <div
+        style={{
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+        className="cursor"
+      ></div>
+      <div
+        style={{
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+        className="cursor-follower"
+      ></div>
+    </div>
+  );
+};
+
+export default Cursor;
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
+import Cursor from "../components/Cursor";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Cursor />
       <Navbar />
       <Component {...pageProps} />
     </>
