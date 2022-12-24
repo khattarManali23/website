e36ebfdf6307e4924e3a087ea1a0a4cb04e9ce66
khattarmanali23 <dev17.technolitics@gmail.com>
2022-12-24 11:59:59 +0530

    changes in career page

--- components/CareerBanner.js
+++ components/CareerBanner.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import girl from "../assets/girl.jpeg";
+import career from "../data/career";
 
 const CareerBanner = () => {
   return (
@@ -8,65 +11,86 @@ const CareerBanner = () => {
         <div id="hero" class="pt-5 lg:flex items-center">
           <div class="px-5 sm:px-10 md:px-10 md:flex lg:block lg:w-1/2 lg:max-w-4xl lg:mr-8 lg:px-20 event h-full">
             <div class="md:w-1/2 md:mr-10 lg:w-full lg:mr-0">
-              <h1 class="text-3xl xl:text-4xl font-black md:leading-none xl:leading-tight">
-                Hire a Designer Near You
+              <h1
+                class="text-3xl xl:text-4xl font-black md:leading-none xl:leading-tight text-transparent bg-clip-text sm:py-4 py-2   leading-8   
+      sm:bg-gradient-to-r sm:from-[#FFB838] sm:via-[#F34F8C] sm:to-[#8236BA]  bg-gradient-to-r from-[#8236BA] via-[#F34F8C] to-[#FFB838]"
+              >
+                Careers @Innodeed{" "}
               </h1>
-              <p class="mt-4 xl:mt-2">
-                World Class Designers, just for you on contract, full-time or
-                part-time, whatever you need.
-              </p>
             </div>
 
-            <div class="flex-1">
-              <div class="relative mt-4 md:mt-0 lg:mt-4">
-                <div class="pl-4 pr-4 h-full absolute bottom-0 left-0 flex items-center">
-                  <svg
-                    class="text-gray-700 w-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <circle cx="11" cy="11" r="8"></circle>
-                    <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                  </svg>
-                </div>
-                <input
-                  type="text"
-                  class="w-full border bg-gray-100 px-4 py-4 text-sm tracking-wide focus:outline-none focus:shadow-outline rounded pl-12"
-                  placeholder="Type (e.g junior, senior)"
-                />
-              </div>
-              <div class="relative mt-4">
-                <div class="pl-4 pr-4 h-full absolute bottom-0 left-0 flex items-center">
-                  <svg
-                    class="text-gray-700 w-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z" />
-                    <circle cx="12" cy="10" r="3" />
-                  </svg>
-                </div>
-                <input
-                  type="text"
-                  class="w-full border bg-gray-100 px-4 py-4 text-sm tracking-wide focus:outline-none focus:shadow-outline rounded pl-12"
-                  placeholder="Mountain View, California"
-                />
-              </div>
-              <div>
-                <button class="transition-all duration-300 mt-5 w-full border border-transparent rounded font-semibold tracking-wide text-sm px-5 py-4 focus:outline-none focus:shadow-outline bg-indigo-500 text-gray-100 hover:bg-indigo-600 hover:text-gray-200">
-                  Find Designers
-                </button>
-              </div>
+            <div class="flex-1 flex flex-col w-full h-full">
+              <Carousel
+                showArrows={false}
+                showThumbs={false}
+                showStatus={false}
+                showIndicators={true}
+                infiniteLoop={true}
+                autoPlay={true}
+                interval={3000}
+                transitionTime={1000}
+                swipeable={true}
+                dynamicHeight={true}
+                emulateTouch={true}
+                stopOnHover={true}
+                useKeyboardArrows={true}
+                centerMode={true}
+                centerSlidePercentage={100}
+                className="flex-1    relative "
+                renderIndicator={(onClickHandler, isSelected, index, label) => {
+                  const defStyle = {
+                    marginLeft: 20,
+                    color: "white",
+                    cursor: "pointer",
+                  };
+                  const style = isSelected
+                    ? {
+                        ...defStyle,
+                        width: 120,
+                        height: 120,
+
+                        FontSize: "100px",
+                        background:
+                          "linear-gradient(to right, #f32170, #ff6b08, #cf23cf, #eedd44)",
+                        WebkitBackgroundClip: "text",
+                        WebkitTextFillColor: "transparent",
+                      }
+                    : {
+                        ...defStyle,
+                        width: 120,
+                        height: 120,
+                        FontSize: "100px",
+                      };
+                  return (
+                    <span
+                      style={style}
+                      onClick={onClickHandler}
+                      onKeyDown={onClickHandler}
+                      value={index}
+                      key={index}
+                      role="button"
+                      tabIndex={0}
+                      aria-label={`${label} ${index + 1}`}
+                      className=" text-[4rem] text-left my-8"
+                    >
+                      •
+                    </span>
+                  );
+                }}
+              >
+                {career.map((item, index) => {
+                  return (
+                    <div class="text-left h-80   " key={index}>
+                      <h5 class="text-3xl font-medium leading-[45px] my-6  ">
+                        {item.title}
+                      </h5>
+                      <p class="text-white leading-[30px] font-normal  text-xl">
+                        {item.content}
+                      </p>
+                    </div>
+                  );
+                })}
+              </Carousel>
             </div>
           </div>
           <div class="mt-6  flex-1 lg:mt-0">
---
+++ data/career.js
@@ -0,0 +1,28 @@
+const career = [
+  {
+    id: 1,
+    title: "Maya Kyon",
+    content:
+      "We develop creative solutions for small and big brands like build artistic products identities We develop creative solutions for small and",
+  },
+  {
+    id: 2,
+    title: "Andrew Jainfer",
+    content:
+      "Recreate the kind of apps that are not just installed but are also used users love or app and so do businesses we help companies bring to life extraordinary experience and mobile moments that other brand fidelity Recreate the kind of apps that are not just installed ",
+  },
+  {
+    id: 3,
+    title: "Maya Kyon",
+    content:
+      "We develop creative solutions for small and big brands like build artistic products identities We develop creative solutions for small and",
+  },
+  {
+    id: 4,
+    title: "Andrew Jainfer",
+    content:
+      "Recreate the kind of apps that are not just installed but are also used users love or app and so do businesses we help companies bring to life extraordinary experience and mobile moments that other brand fidelity Recreate the kind of apps that are not just installed ",
+  },
+];
+
+export default career;
