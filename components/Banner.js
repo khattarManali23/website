8452fce10484c8024b880b0665da767788715286
khattarmanali23 <dev17.technolitics@gmail.com>
2022-12-27 02:12:30 +0530

    chnagrs in home page

--- components/Banner.js
+++ components/Banner.js
@@ -13,29 +13,29 @@ const Banner = ({ banner }) => {
 
     tl.to(
       CSSRulePlugin.getRule("body:before"),
-      0.8,
-      { cssRule: { top: "50%" }, ease: "Power2.easeOut " },
+      0.2,
+      { cssRule: { top: "50%" }, ease: "Power2.easeOut" },
       "close"
     )
       .to(
         CSSRulePlugin.getRule("body:after"),
-        0.8,
+        0.2,
         { cssRule: { bottom: "50%" }, ease: "Power2.easeOut" },
         "close"
       )
       .to(document.querySelector(".loader"), 0.2, { opacity: 1 })
       .to(
         CSSRulePlugin.getRule("body:before"),
-        0.8,
-        { cssRule: { top: "0%" }, ease: "Power2.easeIn" },
-        "+=1.8",
+        0.2,
+        { cssRule: { top: "0%" }, ease: "Power2.easeOut" },
+        "+=1.5",
         "open"
       )
       .to(
         CSSRulePlugin.getRule("body:after"),
-        0.8,
-        { cssRule: { bottom: "0%" }, ease: "Power2.easeIn" },
-        "-=0.4",
+        0.2,
+        { cssRule: { bottom: "0%" }, ease: "Power2.easeOut" },
+        "-=0.2",
         "open"
       )
       .to(document.querySelector(".loader"), 0.2, { opacity: 0 }, "-=0.2");
