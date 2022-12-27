da70c5b2855ddfaa9a974d9bfeea7b9cc4908fa2
khattarmanali23 <dev17.technolitics@gmail.com>
2022-12-27 12:19:42 +0530

    chnagrs in home page

--- components/Banner.js
+++ components/Banner.js
@@ -6,34 +6,34 @@ import React from "react";
 gsap.registerPlugin(CSSRulePlugin);
 
 const Banner = ({ banner }) => {
-  setTimeout(transition, 10);
+  setTimeout(transition, 1000);
 
   function transition() {
     var tl = new gsap.timeline();
 
     tl.to(
       CSSRulePlugin.getRule("body:before"),
-      0.2,
+      0.5,
       { cssRule: { top: "50%" }, ease: "Power2.easeOut " },
       "close"
     )
       .to(
         CSSRulePlugin.getRule("body:after"),
-        0.2,
+        0.5,
         { cssRule: { bottom: "50%" }, ease: "Power2.easeOut" },
         "close"
       )
       .to(document.querySelector(".loader"), 0.2, { opacity: 1 })
       .to(
         CSSRulePlugin.getRule("body:before"),
-        0.2,
+        0,
         { cssRule: { top: "0%" }, ease: "Power2.easeOut" },
         "+=1.5",
         "open"
       )
       .to(
         CSSRulePlugin.getRule("body:after"),
-        0.2,
+        0.5,
         { cssRule: { bottom: "0%" }, ease: "Power2.easeOut" },
         "-=0.2",
         "open"
@@ -42,9 +42,8 @@ const Banner = ({ banner }) => {
   }
 
   return (
-    <div className="">
-      {/* <div class=" "> */}
-      <div class="progress">
+    <div className="w-full h-full flex justify-center items-center flex-col relative">
+      <div class="progress loader w-full">
         <div class="progress-value"></div>
       </div>
       {/* </div> */}
