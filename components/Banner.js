7086ac18664c60ea8101d0498cb0b969167df275
khattarmanali23 <dev17.technolitics@gmail.com>
2022-12-27 11:07:35 +0530

    chnagrs in home page

--- components/Banner.js
+++ components/Banner.js
@@ -44,12 +44,9 @@ const Banner = ({ banner }) => {
   return (
     <div className="">
       <div class="loader ">
-        <div class="bar1"></div>
-        <div class="bar2"></div>
-        <div class="bar3"></div>
-        <div class="bar4"></div>
-        <div class="bar5"></div>
-        <div class="bar6"></div>
+        <div class="progress">
+          <div class="progress-value"></div>
+        </div>
       </div>
       {banner.map((item) => {
         return (
