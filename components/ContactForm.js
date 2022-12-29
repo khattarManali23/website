7677247984dc58d849fc759f74a0ab199b6aa5cb
khattarmanali23 <dev17.technolitics@gmail.com>
2022-12-29 12:58:39 +0530

    seo

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -4,21 +4,23 @@ import contactbg from "../assets/contactbg.jpeg";
 const ContactForm = () => {
   return (
     <div
-      className=" text-gray-100 bg-transparent flex justify-center items-center   py-12"
+      className=" text-gray-100 bg-transparent flex justify-center items-center w-full h-screen   py-12"
       style={{
         backgroundImage: `url(${contactbg.src})`,
         backgroundSize: "cover",
+        position: "relative",
+
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="flex justify-center items-center w-10/12 ">
-        <div className="flex flex-col justify-center items-center flex-1 border border-red-800 w-full">
+      <div className="flex justify-center items-center flex-wrap sm:w-10/12 w-11/10 ">
+        <div className="flex flex-col justify-center items-center flex-1 w-full">
           <div className="w-full">
-            <h3 className="border-white text-left text-[75px]"> Contact us</h3>
+            <h3 className="border-whites text-left text-[75px]"> Contact us</h3>
           </div>
         </div>
-        <div className="bg-gray-800 border flex-1 border-red  flex flex-col justify-center items-center w-[602.18px] h-[734px]  relative bottom-0 right-0 ">
+        <div className="bg-gray-800  flex-1   flex flex-col justify-center items-center w-[602.18px] h-[734px] sm:-bottom-14 sm:right-12 right-0 top-0   sm:absolute relative ">
           <h1
             className=" text-3xl pb-6 mt-2  w-full  font-bold text-center text-transparent bg-clip-text  leading-4
       sm:bg-gradient-to-r sm:from-[#FFB838] sm:via-[#F34F8C] sm:to-[#8236BA]  bg-gradient-to-r from-[#8236BA] via-[#F34F8C] to-[#FFB838]"
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,29 @@
 import Cursor from "../components/Cursor";
 import Navbar from "../components/Navbar";
 // import Splash from "../components/Splash";
+import { DefaultSeo } from "next-seo";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <DefaultSeo
+        title="Innodeed"
+        description="Innodeed is a full-service digital solution partne that helps businesses grow online. We are a team of passionate designers, developers, and marketers who are committed to helping businesses grow online."
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          url: "https://innodeed-nu.vercel.app/",
+          site_name: "Innodeed",
+          images: "https://innodeed-nu.vercel.app/images/Logo.png",
+        }}
+        twitter={{
+          handle: "@innodeed",
+          site: "@innodeed",
+          cardType: "summary_large_image",
+        }}
+      />
+
       {/* <Splash /> */}
       <Cursor />
       <Navbar />
