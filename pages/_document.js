0b7a97f364d99f29c50317b3e40c3b3dbe5e431a
khattarmanali <manalikhattar1@gmail.com>
2023-06-07 13:33:07 +0530

    chges in google tag

--- pages/_document.js
+++ pages/_document.js
@@ -7,20 +7,14 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <script
-          type="text/javascript"
-          async
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w, d, s, l, i) {
-                    w[l] = w[l] || []
-                    w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
-                    const f = d.getElementsByTagName(s)[0],
-                      j = d.createElement(s),
-                      dl = l != "dataLayer" ? "&l=" + l : ""
-                    j.async = true
-                    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
-                    f.parentNode.insertBefore(j, f)
-                  })(window,document,'script','dataLayer',"<GTM-ND5K6VT>")`,
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-ND5K6VT');
+              `,
           }}
         />
       </Head>
