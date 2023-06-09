ff5654a6fb8762416c8555c025746c35a1cf3fd3
khattarmanali <manalikhattar1@gmail.com>
2023-06-09 11:42:40 +0530

    chnge tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,40 @@
+/* eslint-disable @next/next/inline-script-id */
 /* eslint-disable @next/next/next-script-for-ga */
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script
+        {/* <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Y0MJBV6WCF');
+</script> */}
+
+        {/* apply google analytic */}
+
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-Y0MJBV6WCF`}
+        />
+
+        <Script
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-ND5K6VT');
-              `,
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+
+    gtag('config', 'G-Y0MJBV6WCF', {
+      page_path: window.location.pathname,
+    });
+    `,
           }}
         />
       </Head>
